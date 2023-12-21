6402acf4133d793aefc2aa861a0f54bdb6aad577
balaji-githubstore <balajidinakaran1@gmail.com>
2023-12-21 17:39:43 +0530

    Commit 1 Java Code

---
+++ src/com/metlife/print/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.metlife.print;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello 1236666");
+        System.out.print("hello1");
+        System.out.println("hello2");
+
+    }
+
+}
