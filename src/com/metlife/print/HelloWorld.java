69ff63330b912f902a7cab6480155d08ee886642
balaji-githubstore <balajidinakaran1@gmail.com>
2023-12-22 14:33:13 +0530

    Update HelloWorld Java

--- src/com/metlife/print/HelloWorld.java
+++ src/com/metlife/print/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("hello 1236666");
+        System.out.println("hello 12366676");
         System.out.print("hello1");
         System.out.println("hello2");
 
